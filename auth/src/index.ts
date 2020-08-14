b2c4d4bd2997ad1ecafe6e98a9f9edcf22b373eb
marcelom97 <marcelomollaj@gmail.com>
2020-08-14 21:02:51 +0300

    [added] CustomError Abstaction and NotFoundError

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,14 +8,21 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
+// Mount Routes on servers
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// Throws Not Found Error for Routes that not exist
+app.all('*', () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
