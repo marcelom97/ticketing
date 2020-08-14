87fc135455a27f08e474e4323dc6b0141dc3a513
marcelom97 <marcelomollaj@gmail.com>
2020-08-15 01:21:20 +0300

    [added] get current user request

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Hello there'
-  });
+  if (!req.session?.jwt) {
+    return res.status(200).json({ currentUser: null });
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.status(200).json({ currentUser: payload });
+  } catch (err) {
+    res.status(200).json({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
