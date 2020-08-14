b605ca66e53bcaf4cf958ee5cbeb8f938eb7421e
marcelom97 <marcelomollaj@gmail.com>
2020-08-14 22:13:34 +0300

    [added] must have interfaces on User model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,12 +1,5 @@
 import mongoose from 'mongoose';
 
-// An interface that describes the properties
-// that aer erquired to create a new User
-interface UserAttrs {
-  email: string;
-  password: string;
-}
-
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -18,17 +11,37 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', userSchema);
+// An interface that describes the properties
+// that aer erquired to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that  User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that  User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
 
 // Function to create a new User
 // because typecript and mongoose don't cooperate
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
-// new User({
-//   email: 'test@test.com',
-//   password: '123456'
-// });
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+const user = User.build({
+  email: 'test@test.com',
+  password: '123456'
+});
 
 export { User };
