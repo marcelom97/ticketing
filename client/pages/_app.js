6073fb295c25065bd25af377ac069edb6bcd91b1
marcelom97 <marcelomollaj@gmail.com>
2020-08-16 01:56:42 +0300

    [added] pass props throw components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/buildClient';
 
-export default function app({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function AppComponent({ Component, pageProps, currentUser }) {
+  return (
+    <div>
+      <h1>Hello {currentUser.email}</h1>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data
+  };
+};
+
+export default AppComponent;
