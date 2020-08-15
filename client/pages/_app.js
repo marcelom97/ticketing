ba89f1fc65feeb7b253d1328d3359b0a9ab9b23e
marcelom97 <marcelomollaj@gmail.com>
2020-08-15 20:52:36 +0300

    [added] client pages with next.js and bootstrap css

---
+++ client/pages/_app.js
@@ -0,0 +1,5 @@
+import 'bootstap/dist/css/bootstap.css';
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
---
+++ client/pages/index.js
@@ -0,0 +1,4 @@
+const landingPage = () => {
+  return <h1>Landing Page</h1>;
+};
+export default landingPage;
